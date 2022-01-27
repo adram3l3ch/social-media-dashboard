76c4bca89c9f7212075592769822d5494b5e42d8
adramelech <adramelech.psd@gmail.com>
2022-01-27 14:26:11 +0530

    dashboard component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+import MainCard from "./components/mainCard/MainCard";
+import data from "./data.json";
+import "./app.css";
+
 function App() {
-    return <div className="App">hi</div>;
+    return (
+        <main className="app dark-mode">
+            <section className="dashboard">
+                {data.dashboard.map(app => (
+                    <MainCard data={app} />
+                ))}
+            </section>
+        </main>
+    );
 }
 
 export default App;
---
+++ src/components/mainCard/MainCard.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import Facebook from "../../images/icon-facebook.svg";
+import up from "../../images/icon-up.svg";
+import down from "../../images/icon-down.svg";
+import "./maincard.css";
+
+const MainCard = ({ data }) => {
+    return (
+        <article className="maincard instagram" style={{ "--pseudo-bg": data.color }}>
+            <header className="maincard__header">
+                <img src={Facebook} alt="" />
+                <p className="username">{data.username}</p>
+            </header>
+            <section className="maincard__content">
+                <h2>{data.followers}</h2>
+                <p>FOLLOWERS</p>
+            </section>
+            <footer>
+                <img src={data.diff > 0 ? up : down} alt="" />
+                <p className={data.diff > 0 ? "up" : "down"}>{Math.abs(data.diff)} Today</p>
+            </footer>
+        </article>
+    );
+};
+
+export default MainCard;
